da8e295c2e51f571b16ca1fe24dfced82ae727bc
ShyamSunderNT <ntshyamsunder96@gmail.com>
2024-05-28 23:40:10 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,10 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
-});
+})
+
+vite.config.js
